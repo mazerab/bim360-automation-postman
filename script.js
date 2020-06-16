c7f76ef2e7605d2e73fce02675c04a4fa3bf11c3
Bastien Mazeran <bastien.mazeran@autodesk.com>
2020-06-16 16:39:52 -0700

    newman fixes

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const newman = require('newman');
 newman.run({
     collection: require('./postman_collection.json'),
     environment: require('./site_1.postman_environment.json'),
+    folder: ['Authentication', 'Downloads'],
     reporters: 'cli'
 }, function (err) {
     if (err) { throw err; }
@@ -11,7 +12,7 @@ newman.run({
 }).on('request', function (err, execution) { // This is triggered when a response has been recieved
     if (err) { return console.error(err); }
     if (execution.item.name === 'Download') {
-        fs.writeFile(`response${i++}.txt`, execution.response.stream, function (error) {
+        fs.writeFile('model.rvt', execution.response.stream, function (error) {
             if (error) { console.error(error); }
         });
     }
